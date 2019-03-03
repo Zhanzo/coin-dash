3b6d79e712f112f07c75777afae00edc94ef9fa9
Robin Andersson <robin182andersson@gmail.com>
2019-03-03 16:01:05 +0100

    Finished the game and added random obstacle spawning

--- Main.gd
+++ Main.gd
@@ -1,6 +1,8 @@
-extends Node2D
+extends Node
 
 export (PackedScene) var Coin
+export (PackedScene) var Powerup
+export (PackedScene) var Cactus
 export (int) var playTime
 
 var level
@@ -14,7 +16,6 @@ func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
 	
 func new_game():
 	playing = true
@@ -25,17 +26,68 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	spawn_cactus()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
+
+func spawn_cactus():
+	var c = Cactus.instance()
+	$CactusContainer.add_child(c)
+	c.position = Vector2(rand_range(0, screensize.x),
+						rand_range(0, screensize.y))
+	
 	
 func spawn_coins():
+	$LevelSound.play()
 	for i in range(4 + level):
 		var c = Coin.instance()
 		$CoinContainer.add_child(c)
 		c.screensize = screensize
 		c.position = Vector2(rand_range(0, screensize.x),
-		rand_range(0, screensize.y))
+							rand_range(0, screensize.y))
 		
 func _process(delta):
 	if playing and $CoinContainer.get_child_count() == 0:
 		level += 1
 		time_left += 5
 		spawn_coins()
+		spawn_cactus()
+		$PowerupTimer.wait_time = rand_range(5, 10)
+		$PowerupTimer.start()
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+func _on_Player_pickup(type):
+	match type:
+		"coin":
+			score += 1
+			$HUD.update_score(score)
+			$CoinSound.play()
+		"powerup":
+			time_left += 5
+			$PowerupSound.play()
+			$HUD.update_timer(time_left)
+	
+func _on_Player_hurt():
+	game_over()
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
+	$EndSound.play()
+
+
+func _on_PowerupTimer_timeout():
+	var p = Powerup.instance()
+	add_child(p)
+	p.screensize = screensize
+	p.position = Vector2(rand_range(0, screensize.x),
+						 rand_range(0, screensize.y))
